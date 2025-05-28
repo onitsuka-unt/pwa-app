f0725229523f10869866cee6ad5265083b699c26
unT_onitsuka <onitsuka@un-t.com>
2025-05-28 10:18:23 +0900

    .envからAPIキーを取得するように調整

--- src/const/index.ts
+++ src/const/index.ts
@@ -1,4 +1,4 @@
 export const BASE_URL = import.meta.env.MODE === 'development' ? '/' : '/pwa-app/';
 export const CACHE_NAME = 'runtime-cache-v1';
-export const GOOGLE_MAPS_API_KEY = 'AIzaSyD-zUPioEdIdNgVNjh1vxtFA3Be14FsFLw';
-export const GOOGLE_MAPS_ID = '475d0c1718619a9a8a6d680c';
+export const GOOGLE_MAPS_API_KEY = import.meta.env.PUBLIC_GOOGLE_MAPS_API_KEY;
+export const GOOGLE_MAPS_ID = import.meta.env.PUBLIC_GOOGLE_MAPS_ID;
